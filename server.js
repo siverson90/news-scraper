67278a10439a447eb62d6c08b10d3ca2da834f02
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-11-17 21:46:54 -0800

    adding mLab config

--- server.js
+++ server.js
@@ -18,8 +18,10 @@ app.use(logger("dev"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/newscraper", {
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
